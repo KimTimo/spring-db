29788c3071959092d962995b0454e24b304b296b
kimtimo <eogus8875@naver.com>
2024-06-04 00:37:13 +0900

    add : 테스트코드 수정

--- src/test/java/hello/jdbc/service/MemberServiceV1Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -43,21 +43,40 @@ class MemberServiceV1Test {
 
     @Test
     @DisplayName("정상 이체")
-    void accountTransferEx() throws SQLException {
+    void accountTransfer() throws SQLException {
         // given
         Member memberA = new Member(MEMBER_A, 10000);
-        Member memberEx = new Member(MEMBER_EX, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
         memberRepository.save(memberA);
-        memberRepository.save(memberEx);
+        memberRepository.save(memberB);
 
         // when
-        assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
-                .isInstanceOf(IllegalStateException.class);
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
 
         // then
         Member findMemberA = memberRepository.findById(memberA.getMemberId());
-        Member findMemberB = memberRepository.findById(memberEx.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
         assertThat(findMemberA.getMoney()).isEqualTo(8000);
-        assertThat(findMemberB.getMoney()).isEqualTo(10000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12000);
     }
+
+    @Test
+    @DisplayName("이체중 예외 발생")
+    void accountTransferEx() throws SQLException {
+        //given
+         Member memberA = new Member(MEMBER_A, 10000);
+         Member memberEx = new Member(MEMBER_EX, 10000);
+         memberRepository.save(memberA);
+         memberRepository.save(memberEx);
+
+         //when
+         assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                     .isInstanceOf(IllegalStateException.class);
+         //then
+         Member findMemberA = memberRepository.findById(memberA.getMemberId());
+         Member findMemberEx = memberRepository.findById(memberEx.getMemberId());
+        //memberA의 돈만 2000원 줄었고, ex의 돈은 10000원 그대로이다.
+        assertThat(findMemberA.getMoney()).isEqualTo(8000); assertThat(findMemberEx.getMoney()).isEqualTo(10000);
+    }
+
 }
