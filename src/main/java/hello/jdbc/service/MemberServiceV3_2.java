6db3d8fa3e192e2a444f1ec777d3123442ba525d
kimtimo <eogus8875@naver.com>
2024-06-16 02:13:35 +0900

    add : 스프링 트랙잭션, AOP 로직 추가

--- src/main/java/hello/jdbc/service/MemberServiceV3_2.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -2,36 +2,36 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랜잭션 매니저
+ * 트랜잭션 - 트랜잭션 템플릿
  */
 @Slf4j
-@RequiredArgsConstructor
-public class MemberServiceV3_1 {
+public class MemberServiceV3_2 {
 
-    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate template;
     private final MemberRepositoryV3 memberRepository;
 
-    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        // 트랜잭션 시작
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    public MemberServiceV3_2(PlatformTransactionManager manager, MemberRepositoryV3 memberRepository) {
+        this.template = new TransactionTemplate(manager);
+        this.memberRepository = memberRepository;
+    }
 
-        try {
-            bizLogic(fromId, toId, money);
-            transactionManager.commit(status); // 성공 시 커밋
-        } catch (Exception e) {
-            transactionManager.rollback(status);  // 실패 시 롤백
-            throw new IllegalStateException(e);
-        }
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        template.executeWithoutResult((status) -> {
+            // 비즈니스 로직
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
